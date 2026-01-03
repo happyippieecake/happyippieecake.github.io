792e816ff57c3714f64594d4daf557798acae5bc
BAGUS0630 <mbagus0111@gmail.com>
2026-01-03 18:58:21 +0700

    ubah lagi

--- index.php
+++ index.php
@@ -130,10 +130,10 @@ $menus = $conn->query("SELECT * FROM menu ORDER BY id DESC LIMIT 3");
     <div class="max-w-6xl mx-auto px-4">
       <h2 class="text-4xl font-bold text-center mb-10 text-pink-600 brand-font tracking-wide slide-in scroll-animate">Galeri Karya Cake Terbaik</h2>
       <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjS7XjbQ7Eu32S9YAmeju-4QM6whprsavSTQ&s" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjS7XjbQ7Eu32S9YAmeju-4QM6whprsavSTQ&s" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjS7XjbQ7Eu32S9YAmeju-4QM6whprsavSTQ&s" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjS7XjbQ7Eu32S9YAmeju-4QM6whprsavSTQ&s" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
+        <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
+        <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
+        <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
+        <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=400&q=80" class="rounded-2xl shadow-xl object-cover h-40 w-full card-anim fade-in scroll-animate">
       </div>
     </div>
   </section>
